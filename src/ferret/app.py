6a7c6f51d0949bc60523c2365224bc65cc48f0aa
mkaczmarek-xb <157353109+mkaczmarek-xb@users.noreply.github.com>
2024-03-15 11:05:47 +0100

    FP-90/91: Fix UI issues & general cleanup  (#1)
    * moved ferret module to src
    
    * removed obsolete dockerfile
    
    * added pre-commit
    
    * use poetry inside docker; dockerfile rework
    
    * added cmds to README - it's WIP
    
    * ruff format
    
    * FP-91 impl
    
    * ruff
    
    * reverted change in app.py
    
    ---------
    
    Co-authored-by: miki <m.kaczmarek9@gmail.com>

---
+++ src/ferret/app.py
@@ -0,0 +1,30 @@
+import threading
+import os
+
+
+def run_controller():
+    os.system("python -m src.ferret.serve.controller --host 0.0.0.0 --port 10000")
+
+
+controller_thread = threading.Thread(target=run_controller)
+controller_thread.start()
+
+
+def run_gradio_web_server():
+    os.system(
+        "python -m src.ferret.serve.gradio_web_server --controller http://localhost:10000 --model-list-mode reload --add_region_feature"
+    )
+
+
+gradio_web_server = threading.Thread(target=run_gradio_web_server)
+gradio_web_server.start()
+
+
+def run_model_worker():
+    os.system(
+        "CUDA_VISIBLE_DEVICES=0 python -m src.ferret.serve.model_worker --host 0.0.0.0 --controller http://localhost:10000 --port 40000 --worker http://localhost:40000 --model-path ./model/ferret-7b-v1-3 --add_region_feature"
+    )
+
+
+model_worker = threading.Thread(target=run_model_worker)
+model_worker.start()
